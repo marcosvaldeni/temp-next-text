252410fe8ff1bad3c51ae43dca8b0b5aa14b4a26
Marcos Lucas <marcosvaldeni@gmail.com>
2021-03-04 14:44:45 +0000

    added typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main >
+
+        <h1>test</h1>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
+
+export default Home;
